cb005c9990b233c68d1ef66dc3bde038d759cf83
Koharudin <koha001>
2025-02-21 16:40:11 +0700

    add axios

--- app_be/app.js
+++ app_be/app.js
@@ -1,5 +1,6 @@
 // app.js
 const express = require("express")
+const axios = require("axios")
 const app = express()
 const port = 3000
 
@@ -9,4 +10,34 @@ app.get("/", (req, res) => {
 
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`)
+})
+
+app.use(express.json())
+app.post("/create-post", async (req, res) => {
+  try {
+    // Simulate the data to send in the POST request
+    const postData = {
+      title: req.body.title,
+      body: req.body.body,
+      userId: 1,
+    }
+    // Make a POST request using axios
+    const response = await axios.post(
+      "https://jsonplaceholder.typicode.com/posts",
+      postData
+    )
+
+    // Send back the response from the API
+    res.status(201).json({
+      message: "Post created successfully!",
+
+      data: response.data,
+    })
+  } catch (error) {
+    // Handle error and send response
+    res.status(500).json({
+      message: "Error occurred while creating the post",
+      error: error.message,
+    })
+  }
 })
